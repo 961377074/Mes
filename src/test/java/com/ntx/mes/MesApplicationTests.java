02393649d44dd0653df60175766f67e2899edaf9
Seeker <961377074@qq.com>
2022-08-03 09:15:32 +0800

    Initial commit

---
+++ src/test/java/com/ntx/mes/MesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntx.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
